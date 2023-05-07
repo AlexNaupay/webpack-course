f6429baf04c401785e1a7ccd2103a1b6a956f179
Alex Naupay <alexnaupay@gmail.com>
2023-05-06 23:13:55 -0500

    webpack development env

---
+++ webpack.config.dev.js
@@ -0,0 +1,85 @@
+/** @type {import('webpack').Configuration} */
+
+const path = require('path'); // Para trabajar con archivos y rutas de directorios
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+    mode: 'development', // le pasamos explicitamente el modo desde el archivo
+    entry: './src/index.js', // el punto de entrada de mi aplicación
+    output: { // Esta es la salida de mi bundle
+        path: path.resolve(__dirname, 'dist'),
+        // resolve lo que hace es darnos la ruta absoluta de el S.O hasta nuestro archivo
+        // para no tener conflictos entre Linux, Windows, etc
+        //filename: 'main.js', // EL NOMBRE DEL ARCHIVO FINAL,
+        filename: '[name].js',
+
+        assetModuleFilename: 'assets/images/[name][ext][query]'
+    },
+    resolve: {
+        extensions: ['.js'], // LOS ARCHIVOS QUE WEBPACK VA A LEER
+        alias: {
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@templates': path.resolve(__dirname, 'src/templates/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@images': path.resolve(__dirname, 'src/assets/images/'),
+        }
+    },
+
+    module: {
+        rules: [
+            {
+                // Test declara que extensión de archivos aplicara el loader
+                test: /\.m?js$/,
+                // Exclude permite omitir archivos o carpetas especificas
+                exclude: /node_modules/,
+                // Use es un arreglo u objeto donde dices que loader aplicaras
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test: /\.css|\.styl$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {filename: 'static/images/[name][ext][query]'},
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/fonts/[name][ext]",
+                },
+            }
+        ],
+
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            //title: 'Title of my page',
+            inject: 'body',
+            template: './public/index.html',
+            filename: 'index.html',
+            // minify: false,
+            // xhtml: false,
+            // hash: false, // If true then append a unique webpack compilation hash to all included scripts and CSS files. This is useful for cache busting
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css'
+        }),
+        new CopyPlugin({ // CONFIGURACIÓN DEL COPY PLUGIN
+            patterns: [
+                {
+                    from: path.resolve(__dirname , "src" , 'assets/images'), // CARPETA A MOVER AL DIST
+                    to: "assets/images" // RUTA FINAL DEL DIST
+                }
+            ]
+        }),
+        new Dotenv()
+    ],
+}
